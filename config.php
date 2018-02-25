cc05d524e19ac4351980b53a41c418aa5c54ddce
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-02-25 11:14:30 +0000

    Azure connection files

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$db = mysqli_init();
+mysqli_ssl_set($db,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+mysqli_real_connect($db, 'compgc06group35db.mysql.database.azure.com', 'compgc06group35@compgc06group35db', 'Aakash123', 'auctionwebsite', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+if (mysqli_connect_errno($db)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+?>
