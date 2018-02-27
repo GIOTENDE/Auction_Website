c19c101674a5bd7d023534c7316f75c13890ad6d
GIOTENDE <giov.tenderini@gmail.com>
2018-02-27 13:27:08 +0000

    PHP signup

--- Signin.php
+++ Signin.php
@@ -1,5 +1,30 @@
 <html lang="en-Us">
-<?php require '../Auction_Website/includes/pagetop.php'; ?>
+<?php require '../Auction_Website/includes/pagetop.php'; 
+
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT userID FROM users WHERE username = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
 <head>
 
 	<meta charset="utf-8">
@@ -24,9 +49,9 @@
 
 			<fieldset>
 
-				<p><input type="text" required value="Username" onBlur="if(this.value=='')this.value='Username'" onFocus="if(this.value=='Username')this.value='' "></p> <!-- JS because of IE support; better: placeholder="Username" -->
+				<p><input type="text" required value="Username" onBlur="if(this.value=='')this.value='Username'" onFocus="if(this.value=='Username')this.value='' " name="username"></p> <!-- JS because of IE support; better: placeholder="Username" -->
 
-				<p><input type="password" required value="Password" onBlur="if(this.value=='')this.value='Password'" onFocus="if(this.value=='Password')this.value='' "></p> <!-- JS because of IE support; better: placeholder="Password" -->
+				<p><input type="password" required value="Password" onBlur="if(this.value=='')this.value='Password'" onFocus="if(this.value=='Password')this.value='' " name="password"></p> <!-- JS because of IE support; better: placeholder="Password" -->
 
 		
 
--- Signup.php
+++ Signup.php
@@ -2,6 +2,12 @@
 
 <?php 
 require '../Auction_Website/includes/pagetop.php'; 
+$db = mysqli_init();
+mysqli_ssl_set($db,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+mysqli_real_connect($db, 'compgc06group35db.mysql.database.azure.com', 'compgc06group35@compgc06group35db', 'Aakash123', 'auctionwebsite', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+if (mysqli_connect_errno($db)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 session_start();
 $_SESSION['message'] = '';
 if($_SERVER['REQUEST METHOD'] == 'POST'){
@@ -87,14 +93,10 @@ if($_SERVER['REQUEST METHOD'] == 'POST'){
 		</form>
 
         <p><br>
-          
-          
-          
-          
-          
-          
+
           
   <button class="facebook">Sign Up</button>
+  <br>
           </p>
 	</div> <!-- end login -->
 
