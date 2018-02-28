be87ba4e559a2f14673a924f17b0a4f77f3cafce
GIOTENDE <giov.tenderini@gmail.com>
2018-02-28 12:56:43 +0000

    hiuhuh

--- Signup.php
+++ Signup.php
@@ -1,36 +1,37 @@
 
 <?php 
-session_start();
-$db = mysqli_init();
-mysqli_ssl_set($db,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
-mysqli_real_connect($db, 'compgc06group35db.mysql.database.azure.com', 'compgc06group35@compgc06group35db', 'Aakash123', 'auctionwebsite', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
-if (mysqli_connect_errno($db)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+include("config.php");
+
+
 
 $_SESSION['message'] = '';
-if($_SERVER['REQUEST METHOD'] == 'POST'){
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	if(isset($_POST["submit"])){
 	// two passwords are matching
 	if($_POST['password'] == $_POST['confirmpassword']){
-		$username = $mysql->real_escape_string($_POST['username']);
-		$email_address = $mysql->real_escape_string($_POST['email_address']);
+		$username = ($_POST['username']);
+		$email_address = ($_POST['email_address']);
 		$password = md5($_POST['password']);// md5 hash passord security
-		$fullname = $mysql->real_escape_string($_POST['fullname']);
-		$mobilenumber = $mysql->real_escape_string($_POST['mobilenumber']);
-		$address = $mysql->real_escape_string($_POST['address']);
-		$Seller_or_Buyer= $mysql->real_escape_string($_POST['Seller_or_Buyer']);
+		$fullname = ($_POST['fullname']);
+		$mobilenumber = ($_POST['mobilenumber']);
+		$address = ($_POST['address']);
+		$Seller_or_Buyer= ($_POST['Seller_or_Buyer']);
 		$sql = "INSERT INTO users (username, email_address, password, fullname, mobilenumber, address, Seller_or_Buyer)"."VALUES ('$username','$email_address','$password','$fullname','$mobilenumber','$address','$Seller_or_Buyer')";
-		//if query is successful, redirect to welcome.php page, done!
-		if ($msqli->query($sql)=== true){
+		//if query is successful, redirect to signup.php page, done!
+		
+		mysqli_query($db,$sql);
+			
+		/*if ($mysqli->query($sql)=== true){
 			$_SESSION['message'] = 'Registration successful! Added $username to the database';
 		}
 		else{
 			$_SESSION['message'] = "User could not be added to the database!";
-		}
+		}*/
 	}
 	else{
 		$_SESSION['message'] = "Two Passwords to not match";
-	}
+	} 
+	}else{echo "subbmit not working";}
 }
 
 require '../Auction_Website/includes/pagetop.php'; 
@@ -95,7 +96,7 @@ require '../Auction_Website/includes/pagetop.php';
         <p><br>
 
           
-  <button class="facebook">Sign Up</button>
+  <input type="submit" name="submit" id="submit" class="button" value="Sign Up">
   <br>
           </p>
 	</div> <!-- end login -->
