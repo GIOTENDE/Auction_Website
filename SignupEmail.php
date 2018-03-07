7b6980fb8d9ecd56bdbd455b6a8e4980438980a4
donalmclaughlin <donal.mclaughlin.17@ucl.ac.uk>
2018-03-07 20:07:20 +0000

    email

--- SignupEmail.php
+++ SignupEmail.php
@@ -1,9 +1,12 @@
 <?php
 include("config.php");
 
+$url = 'https://api.sendgrid.com/';
+ $user = 'Group35Email';
+ $pass = 'Group35Email';
+
 $to = $email_address;
 $subject = "Welcome to Auction Website";
-
 $message = "
 <html>
 <head>
@@ -29,13 +32,46 @@ Role: <?php echo $Seller_or_Buyer ?> <br>
 ";
 
 // Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+// $headers = "MIME-Version: 1.0" . "\r\n";
+// $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-// More headers
-$headers .= 'From: <noreply@auction_webste.com>' . "\r\n";
+// // More headers
+// $headers .= 'From: <noreply@auction_website.com>' . "\r\n";
 //$headers .= 'Cc: myboss@example.com' . "\r\n";
 
-mail($to,$subject,$message,$headers);
+//mail($to,$subject,$message,$headers);
+
+ $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      'to' => $email_address,
+      'subject' => $subject,
+      'html' => $message,
+      'text' => 'testing body',
+      'from' => 'noreply@auction_website.com',
+   );
+
+ $request = $url.'api/mail.send.json';
+
+ // Generate curl request
+ $session = curl_init($request);
+
+ // Tell curl to use HTTP POST
+ curl_setopt ($session, CURLOPT_POST, true);
+
+ // Tell curl that this is the body of the POST
+ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+ // Tell curl not to return headers, but do return the response
+ curl_setopt($session, CURLOPT_HEADER, false);
+ curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+ // obtain response
+ $response = curl_exec($session);
+ curl_close($session);
+
+ // print everything out
+ print_r($response);
+
 echo "DONE";
 ?>
