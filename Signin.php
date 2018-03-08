9946117f1dfa1003de84b7f2f9aac58d9b6247f9
donalmclaughlin <donal.mclaughlin.17@ucl.ac.uk>
2018-03-08 15:21:16 +0000

    d

--- Signin.php
+++ Signin.php
@@ -16,6 +16,7 @@
    if($_SERVER["REQUEST_METHOD"] == "POST") {
 
    if(isset($_POST["submit"])){
+
 	if (empty($_POST["username"])) {
 		$usernameErr = "Please Enter Your Username"."<br>";
 		$postCheck = false;
@@ -47,7 +48,39 @@
 }
 }else{
 	echo "hello";
-	echo "<div id='myModal' class='modal'>
+	echo "
+
+	<script>
+  window.onload = function(){
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById('submit');
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName('close')[0];
+  modal.style.display = 'block';
+  // When the user clicks the button, open the modal 
+  // btn.onclick = function() {
+  //     modal.style.display = 'block';
+  // }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = 'none';
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = 'none';
+      }
+  }
+}
+  </script>
+	
+	<div id='myModal' class='modal'>
 
     <!-- Modal content -->
     <div class='modal-content'>
