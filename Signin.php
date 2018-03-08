3693a0e3b6a690b8dd8ce0e9390ff0134ec2c300
donalmclaughlin <donal.mclaughlin.17@ucl.ac.uk>
2018-03-08 12:08:34 +0000

    include

--- Signin.php
+++ Signin.php
@@ -68,7 +68,7 @@
          $error = "Your Login Name or Password is invalid";
       }
    }*/
-require '../Auction_Website/includes/pagetop.php';
+include '../Auction_Website/includes/pagetop.php';
 ?>
 <html lang="en-Us">
 <head>
@@ -119,5 +119,5 @@ require '../Auction_Website/includes/pagetop.php';
 	</div> <!-- end login -->
 
 </body>
-<?php require '../Auction_Website/includes/footer.php'; ?>
+<?php include '../Auction_Website/includes/footer.php'; ?>
 </html>
