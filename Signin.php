df4b824db3795a6bdefd7fa20acc11abafd1f4b7
donalmclaughlin <donal.mclaughlin.17@ucl.ac.uk>
2018-03-08 15:24:38 +0000

    s

--- Signin.php
+++ Signin.php
@@ -48,36 +48,6 @@
 }else{
 	echo "hello";
 	echo "
-
-	<script>
-  window.onload = function(){
-  // Get the modal
-  var modal = document.getElementById('myModal');
-  
-  // Get the button that opens the modal
-  var btn = document.getElementById('submit');
-  
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName('close')[0];
-  modal.style.display = 'block';
-  // When the user clicks the button, open the modal 
-  // btn.onclick = function() {
-  //     modal.style.display = 'block';
-  // }
-  
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modal.style.display = 'none';
-  }
-  
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = 'none';
-      }
-  }
-}
-  </script>
 	
 	<div id='myModal' class='modal'>
 
@@ -204,4 +174,4 @@ include '../Auction_Website/includes/pagetop.php';
       }
   }
 }
-  </script>;
+  </script>
