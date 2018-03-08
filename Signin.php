da49fa225a6b8787809958c218db88827b095a76
donalmclaughlin <donal.mclaughlin.17@ucl.ac.uk>
2018-03-08 15:07:39 +0000

    validation

--- Signin.php
+++ Signin.php
@@ -5,6 +5,7 @@
 
    $postCheck=true;
    $username=$password="";
+
    function validate_input($data) {
 	$data = trim($data);
 	$data = stripslashes($data);
@@ -44,6 +45,26 @@
 		header('Location: roleSeperator.php');
    }
 }
+}else{
+	echo "<div id='myModal' class='modal'>
+
+    <!-- Modal content -->
+    <div class='modal-content'>
+      <div class='modal-header'>
+        <span class='close'>&times;</span>
+        <h2>Incorrect Login details</h2>
+      </div>
+      <div class='modal-body'>
+        <p>
+        Please Re-enter your login details
+        </p>
+        <div class= 'modal-footer'>
+       </div>
+      </div>
+      
+    </div>
+  
+  </div>";
 }
 }     
 }
@@ -121,3 +142,32 @@ include '../Auction_Website/includes/pagetop.php';
 </body>
 <?php include '../Auction_Website/includes/footer.php'; ?>
 </html>
+<script>
+  window.onload = function(){
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById('submit');
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName('close')[0];
+  modal.style.display = 'block';
+  // When the user clicks the button, open the modal 
+  // btn.onclick = function() {
+  //     modal.style.display = 'block';
+  // }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = 'none';
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = 'none';
+      }
+  }
+}
+  </script>;
