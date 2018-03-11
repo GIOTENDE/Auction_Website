8e643c555fbefdf119e5ad99c31e13e0f00b3c48
GIOTENDE <giov.tenderini@gmail.com>
2018-03-11 23:11:43 +0000

    css on point

--- Seller/SellerProfile/SellerProfile.php
+++ Seller/SellerProfile/SellerProfile.php
@@ -30,15 +30,11 @@ $getProductDetails = mysqli_query($db,"SELECT prod_id, prod_name, prod_start_dat
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="#">Project name</a>
         </div>
-        <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
             <li href="../../logout.php" class="active"><a>Logout</a></li>
