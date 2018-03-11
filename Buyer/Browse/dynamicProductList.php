ff31359369f1871943ba2ffb67ba037ce6dc496e
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-03-11 21:24:35 +0000

    Product page watch list button ajax functions

--- Buyer/Browse/dynamicProductList.php
+++ Buyer/Browse/dynamicProductList.php
@@ -3,11 +3,9 @@
 <script type="text/javascript"
         src="dynamicProductListController.js">
 </script>
-
-
 <?php
 
-$productSearch =  $_POST["search"];
+$productSearch = $_POST["search"];
 
 $category2 = $_POST["searchCategories"];
 
@@ -22,36 +20,35 @@ echo $category1;
 
 <div id="container">
 
-    <select name="Name Alphabetical" id="nameAlphabetical"  onclick="sortName()">
+    <select name="Name Alphabetical" id="nameAlphabetical" onclick="sortName()">
         <option value="" disabled selected>Name</option>
         <option value="Name Alphabetical">Name Alphabetical</option>
     </select>
 
-    <select name="Ending soon" id="endingSoon"  onclick="endingSoon(value)">
+    <select name="Ending soon" id="endingSoon" onclick="endingSoon(value)">
         <option value="" disabled selected>Sort by End Date</option>
         <option value="End soon first">End soon first</option>
         <option value="End soon last">End soon last</option>
     </select>
 
-    <select name="Condition Filter" id="condition"  onclick="filterCategory()">
+    <select name="Condition Filter" id="condition" onclick="filterCategory()">
         <option value="" disabled selected>Condition</option>
         <option value="e">New or Used</option>
         <option value="New">New</option>
         <option value="Used">Used</option>
     </select>
 
-    <select name="Reserve price" id="reservePrice"  onclick="sortReservePrice(value)">
+    <select name="Reserve price" id="reservePrice" onclick="sortReservePrice(value)">
         <option value="" disabled selected>Sort Reserve Price</option>
         <option value="High to Low">High to Low</option>
         <option value="Low to High">Low to High</option>
     </select>
 
-    <select name="Highest bid" id="highestBid"  onclick="sortHighestBid(value)">
+    <select name="Highest bid" id="highestBid" onclick="sortHighestBid(value)">
         <option value="" disabled selected>Sort Highest Bid</option>
         <option value="High to Low">High to Low</option>
         <option value="Low to High">Low to High</option>
     </select>
-
 
 
 </div>
@@ -59,37 +56,21 @@ echo $category1;
 
 <div id="container2">
 
+    <?php
+    if (isset($category1)) {
 
-
-
-
-
-
-
-
-<?php
-if(isset($category1)) {
-
-    $sql = "SELECT prod_name, prod_category, prod_description, prod_start_price, prod_reserve_price, prod_end_date, prod_condition, prod_picture, prod_highest_price FROM product WHERE prod_category = '$category1' AND '$dateNow' <= prod_end_date";
-    echo "first loop";
-}
-
-
-// run search for all categories
-else if ($category2 == "*") {
-        $sql = "SELECT prod_name, prod_category, prod_description, prod_start_price, prod_reserve_price, prod_end_date, prod_condition, prod_picture, prod_highest_price FROM product WHERE prod_name LIKE '%$productSearch%' AND '$dateNow' <= prod_end_date";
-    echo "second loop";
-    }
-
-// run search within a category
-else {
+        $sql = "SELECT prod_ID, prod_name, prod_category, prod_description, prod_start_price, prod_reserve_price, prod_end_date, prod_condition, prod_picture, prod_highest_price FROM product WHERE prod_category = '$category1' AND '$dateNow' <= prod_end_date";
+    } // run search for all categories
+    else if ($category2 == "*") {
+        $sql = "SELECT prod_ID, prod_name, prod_category, prod_description, prod_start_price, prod_reserve_price, prod_end_date, prod_condition, prod_picture, prod_highest_price FROM product WHERE prod_name LIKE '%$productSearch%' AND '$dateNow' <= prod_end_date";
+    } // run search within a category
+    else {
 
         $sql = "SELECT prod_ID, prod_name, prod_category, prod_description, prod_start_price, prod_reserve_price, prod_end_date, prod_condition, prod_picture, prod_highest_price FROM product WHERE (prod_name LIKE '%$productSearch%') AND (prod_category = '$category2')  AND '$dateNow' <= prod_end_date";
-        echo "third loop";
     }
-$result = mysqli_query($db, $sql);
+    $result = mysqli_query($db, $sql);
 
-if (mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) > 0) {
     // output data of each row
     ?>
     <table id="myTable">
@@ -104,35 +85,36 @@ if (mysqli_num_rows($result) > 0) {
         </tr>
 
         <?php
-    while($row = mysqli_fetch_assoc($result)) {
-        $prod_ID = $row["prod_ID"];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $prod_ID = $row["prod_ID"];
+            ?>
+
+            <tr>
+                <td> <?php $picture1 = $row["prod_picture"];
+                    $picture2 = "Images/" . $picture1; ?>
+                    <a href="../product/productPage.php?prod_ID=<?php echo $prod_ID; ?>">
+                        <img src="<?php echo $picture2 ?>" alt="Image picture" width="300" height="200">
+                    </a>
+                </td>
+                <td><?php echo $row["prod_name"]; ?> </td>
+                <td><?php echo $row["prod_category"]; ?> </td>
+                <td><?php echo $row["prod_end_date"]; ?> </td>
+                <td><?php echo $row["prod_condition"]; ?> </td>
+                <td><?php echo $row["prod_reserve_price"]; ?> </td>
+                <td><?php echo $row["prod_highest_price"]; ?> </td>
+
+            </tr>
+            <br>
+        <?php }
         ?>
-        <tr>
-            <td> <?php $picture1 = $row["prod_picture"];
-                $picture2 = "Images/" . $picture1;  ?>
-                <a href="../product/productPage.php?prod_ID=<?php echo $prod_ID; ?>">
-                    <img src= "<?php echo $picture2?>" alt="Image picture" width="300" height="200">
-                </a>
-            </td>
-            <td><?php echo    $row["prod_name"]; ?> </td>
-            <td><?php echo $row["prod_category"]; ?> </td>
-            <td><?php echo $row["prod_end_date"]; ?> </td>
-            <td><?php echo $row["prod_condition"]; ?> </td>
-            <td><?php echo  $row["prod_reserve_price"]; ?> </td>
-            <td><?php echo  $row["prod_highest_price"]; ?> </td>
 
-        </tr>
-        <br>
- <?php   }
-    ?>
+        <?php
+        } else {
+            echo "0 results";
+        }
 
-    <?php
-} else {
-    echo "0 results";
-}
-
-mysqli_close($db);
-?>
+        mysqli_close($db);
+        ?>
 
 </div>
 
--- Buyer/product/productPage.php
+++ Buyer/product/productPage.php
@@ -2,37 +2,38 @@
 
 <?php
 session_start();
-//$userID = $_SESSION['userID'];
+//$userID = $_SESSION['userID']; // uncomment this
 $userID = 1;
-$onWishlist = False;
+$onWatchlist = False;
 $prod_ID = $_GET["prod_ID"];
 $sql = "SELECT prod_name, prod_category, prod_description, prod_start_price, prod_reserve_price, prod_end_date, prod_condition, prod_picture FROM product WHERE prod_ID = '$prod_ID'";
 $result = mysqli_query($db, $sql);
 
-    while($row = mysqli_fetch_assoc($result)) {
-        $prod_name = $row["prod_name"];
-        $prod_category = $row["prod_category"];
-        $prod_description = $row["prod_description"];
-        $prod_start_price = $row["prod_start_price"];
-        $prod_reserve_price = $row["prod_reserve_price"];
-        $prod_end_date = $row["prod_end_date"];
-        $prod_condition = $row["prod_condition"];
-        $prod_picture_directory = $row["prod_picture"];
-        $prod_picture = "Images/" . $prod_picture_directory; //needs changing?
-    }
+while ($row = mysqli_fetch_assoc($result)) {
+    $prod_name = $row["prod_name"];
+    //$prod_category = $row["prod_category"];
+    $prod_description = $row["prod_description"];
+    $prod_start_price = $row["prod_start_price"];
+    $prod_reserve_price = $row["prod_reserve_price"];
+    $prod_end_date = $row["prod_end_date"];
+    $prod_condition = $row["prod_condition"];
+    $prod_picture_directory = $row["prod_picture"];
+    $prod_picture = "../Browse/Images/" . $prod_picture_directory; //needs changing?
+}
 
-    $sql2 = "SELECT buyer_ID, prod_ID FROM wishlist WHERE buyer_ID = '$userID' AND prod_ID = '$prod_ID'";
-    $result = mysqli_query($db, $sql2);
+$sql2 = "SELECT buyer_ID, prod_ID FROM watchlist WHERE buyer_ID = '$userID' AND prod_ID = '$prod_ID'";
+$result = mysqli_query($db, $sql2);
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        $onWishlist = True;
-    }
+while ($row = mysqli_fetch_assoc($result)) {
+    $onWatchlist = True;
+}
 mysqli_close($db);
 
 date_default_timezone_set('Europe/London');
 $end_date = new DateTime($prod_end_date);
 $now = new DateTime("now");
 $interval = $now->diff($end_date);
+
 ?>
 
 <!DOCTYPE html>
@@ -42,6 +43,30 @@ $interval = $now->diff($end_date);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('#watchlist').click(function () {
+                if ($(this).html() == 'Save to watch list <span class="glyphicon glyphicon-heart"></span>') {
+                    $.ajax({ url: 'watchlist.php',
+                        data: {action: 'save', 'buyer_ID': 1, 'prod_ID': <?php echo $prod_ID ?>},
+                        type: 'post',
+                        success: function(output) {
+                        }
+                    });
+                    $(this).html('Remove from watch list <span class="glyphicon glyphicon-heart"></span>');
+                } else {
+                    $.ajax({ url: 'watchlist.php',
+                        data: {action: 'remove', 'buyer_ID': 1, 'prod_ID': <?php echo $prod_ID ?>},
+                        type: 'post',
+                        success: function(output) {
+                        }
+                    });
+                    $(this).html('Save to watch list <span class="glyphicon glyphicon-heart"></span>');
+                }
+            })
+        })
+    </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
@@ -52,17 +77,19 @@ $interval = $now->diff($end_date);
     <div class="row">
         <div class="col-sm-4">
             <img src="<?php echo $prod_picture; ?>" class="img-rounded img-responsive">
-            <button type="button" class="btn btn-warning"><?php echo $onWishlist? 'Remove from wish list': 'Save to wish list' ?> <span class="glyphicon glyphicon-heart"></button>
+            <button type="button" id="watchlist"
+                    class="btn btn-warning"><?php echo $onWatchlist ? 'Remove from watch list' : 'Save to watch list' ?> <span class="glyphicon glyphicon-heart"></button>
         </div>
         <div class="col-sm-4"><?php echo $prod_description ?></div>
-        <div class="col-sm-4 well"> <table style="width:100%">
+        <div class="col-sm-4 well">
+            <table style="width:100%">
                 <tr align="center">
                     <td>Condition:</td>
                     <td><b><?php echo $prod_condition; ?></b></td>
                 </tr>
                 <tr align="center">
                     <td>Time remaining:</td>
-                    <td><?php echo $interval->format(' %d days, %h hours, %m minutes')?></td>
+                    <td><?php echo $interval->format(' %d days, %h hours, %m minutes') ?></td>
                 </tr>
             </table>
 
@@ -72,7 +99,7 @@ $interval = $now->diff($end_date);
 </div>
 <hr>
 
-<h2>Customers who have bidded on this item have also bidded on</h2>
-
+<h2>Customers have also bidded on</h2>
+//SELECT DISTINCT prod_id FROM bids WHERE buyer_id IN (SELECT buyer_id FROM bids WHERE prod_id = 58) ORDER BY bid_id DESC LIMIT 3
 </body>
 </html>
