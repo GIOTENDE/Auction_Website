cc9a1c348313bd0b7e997ceeec9ac5613fde3eea
donalmclaughlin <donal.mclaughlin.17@ucl.ac.uk>
2018-03-12 17:13:51 +0000

    logout

--- includes/pagetop.php
+++ includes/pagetop.php
@@ -13,7 +13,7 @@
         <div class="topnavbar-collapse collapse">
           <ul class="topnav navbar-nav">
             <li class="topactive"><a>Home</a></li>
-            <li class="topactive"><a href="../logout.php">Logout</a></li>
+            <li class="topactive"><a href="https://compgc06group35.azurewebsites.net/logout.php">Logout</a></li>
           </ul>
         </div>
       </div>
