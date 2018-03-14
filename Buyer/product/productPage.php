1d5622283fd32160fc75a37ef5d47c449bfc0476
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-03-14 18:39:48 +0000

    Users are notified when they are outbidded.

--- Buyer/product/productPage.php
+++ Buyer/product/productPage.php
@@ -154,7 +154,6 @@ mysqli_close($db);
                         // $('#modal-title').html('Alert');
                         $('#modal-body2').html('The price has been updated as a new bid of £ ' + output + ' has been received.');
                         $('#myModal2').modal('show');
-                        window.location.reload();
                     }
                 }
             });
