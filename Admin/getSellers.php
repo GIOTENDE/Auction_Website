6a3f49141dbdbeadfc3c026ef7a939a6c1eae45e
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-03-15 14:09:40 +0000

    Admin send emails

--- Admin/getSellers.php
+++ Admin/getSellers.php
@@ -1,5 +1,9 @@
 <?php include '../config.php';
 
 //sql statement needed here
-
-include 'advertiseEmail.php';
+$getBuyers = "SELECT email_address FROM users WHERE Seller_or_Buyer = 'Buyer'";
+$result = mysqli_query($db, $getBuyers);
+while ($row = mysqli_fetch_assoc($result)) {
+    $email_address = $row["email_address"];
+    include 'advertiseEmail.php';
+}
