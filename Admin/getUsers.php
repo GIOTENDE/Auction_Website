4da38d255c26eec145e71c561022ce644692c950
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-03-15 16:25:32 +0000

    Admin send emails

--- Admin/getUsers.php
+++ Admin/getUsers.php
@@ -1,10 +1,9 @@
 <?php include '../config.php';
 
-//sql statement needed here
 $getUsers = "SELECT Seller_or_Buyer, fullName, email_address FROM users";
 $result = mysqli_query($db, $getUsers);
 while ($row = mysqli_fetch_assoc($result)) {
-    $role = $row["Seller_or_Buyers"];
+    $role = $row["Seller_or_Buyer"];
     $fullName = $row["fullName"];
     $email_address = $row["email_address"];
 
