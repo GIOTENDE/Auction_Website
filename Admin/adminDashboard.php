d892120213c5a13557fbaada10566f3a0439053b
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-03-15 06:34:17 +0000

    Admin send emails

--- Admin/adminDashboard.php
+++ Admin/adminDashboard.php
@@ -12,6 +12,7 @@ include ("../config.php");
     <script>
         $(document).ready(function() {
             $('#advertisebtn').click(function () {
+                <?php include 'advertiseEmail.php' ?>
                 $.ajax({
                     url: 'advertiseEmail.php',
                     data: {},
