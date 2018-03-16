8cdce8b978f58f409d7e057172771fc391eb8ab6
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-03-16 06:13:03 +0000

    fixed bugs

--- Buyer/BuyerProfile/BuyerHistoricAuctions.php
+++ Buyer/BuyerProfile/BuyerHistoricAuctions.php
@@ -20,7 +20,7 @@ include '../../config.php';
                 var feedback = $("#score").val();
                 $.ajax({
                     url: 'postFeedbackScore.php',
-                    data: {'buyerID': '1', 'feedback': '1', 'prodID': '1'},
+                    data: {'buyerID': <?php echo $userID ?>, 'feedback': '1', 'prodID': '1'},
                     type: 'post',
                     success: function (output) {
                         alert("Feedback sent!")
