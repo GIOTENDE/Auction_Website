2502dbb73e0662131779e1352f92405850506835
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-03-16 06:10:19 +0000

    fixed bugs

--- Buyer/BuyerProfile/BuyerHistoricAuctions.php
+++ Buyer/BuyerProfile/BuyerHistoricAuctions.php
@@ -20,7 +20,7 @@ include '../../config.php';
                 var feedback = $("#score").val();
                 $.ajax({
                     url: 'postFeedbackScore.php',
-                    data: {'buyerID': <?php echo $userID?>, 'feedback': $("#score").val(), 'prodID': <?php echo $prodID?>},
+                    data: {'buyerID': '1', 'feedback': '1', 'prodID': '1'},
                     type: 'post',
                     success: function (output) {
                         alert("Feedback sent!")
--- Buyer/BuyerProfile/postFeedbackScore.php
+++ Buyer/BuyerProfile/postFeedbackScore.php
@@ -1,3 +1,7 @@
 <?php include '../../config.php';
 
-mysqli_query($db,"INSERT INTO feedback (prod_id, buyer_id, seller_id, buyer_feedback_points, seller_feedback_points) VALUES('1', '1', NULL, NULL, '1') ON DUPLICATE KEY UPDATE buyer_id= '1', '1'");
+$postFeedbackScore = mysqli_query($db,"INSERT INTO feedback 
+(prod_id, buyer_id, seller_id, buyer_feedback_points, 
+seller_feedback_points)
+VALUES(" . $_POST['prodID']. ", ". $_POST['buyerID'] .", NULL, NULL, ". $_POST['feedback'] .") ON DUPLICATE KEY UPDATE
+buyer_id= ". $_POST['buyerID'] .", seller_feedback_points= ". $_POST['feedback'] ." ");
