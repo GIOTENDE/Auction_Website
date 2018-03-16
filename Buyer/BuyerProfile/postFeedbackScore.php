d46445ca6adcf8ba99853186d11c543d3c7e2443
Aakash Bhalla <aakash.bhalla.13@ucl.ac.uk>
2018-03-16 06:07:59 +0000

    fixed bugs

--- Buyer/BuyerProfile/postFeedbackScore.php
+++ Buyer/BuyerProfile/postFeedbackScore.php
@@ -1,9 +1,3 @@
-<?php
+<?php include '../../config.php';
 
-include '../../config.php';
-
-$postFeedbackScore = mysqli_query($db,"INSERT INTO feedback 
-(prod_id, buyer_id, seller_id, buyer_feedback_points, 
-seller_feedback_points)
-VALUES('1', '1', NULL, NULL, '1') ON DUPLICATE KEY UPDATE
-buyer_id= '1', '1'");
+mysqli_query($db,"INSERT INTO feedback (prod_id, buyer_id, seller_id, buyer_feedback_points, seller_feedback_points) VALUES('1', '1', NULL, NULL, '1') ON DUPLICATE KEY UPDATE buyer_id= '1', '1'");
